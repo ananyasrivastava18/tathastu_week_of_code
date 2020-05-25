f480670b4a9ec6d5065bde9a0c9aee50e742432d
ananyasrivastava18 <65857467+ananyasrivastava18@users.noreply.github.com>
2020-05-25 11:04:05 +0530

    Create Program5.py

---
+++ Program5.py
@@ -0,0 +1,16 @@
+
+p1=int(input("Enter runs"))
+p2=int(input("Enter runs"))
+p3=int(input("Enter runs"))
+str1=p1*100/60
+str2=p2*100/60
+str3=p3*100/60
+print("Strike rate of 1st=",str1)
+print("strike rate of 2nd=",str2)
+print("Strike rate of 3rd=",str3)
+print("Score of 1st=",p1*2)
+print("Score of 2nd=",p2*2)
+print("Score of 3rd=",p3*2)
+print("Max. no of sixes by 1st:",p1//6)
+print("Max no. of sixes of 2nd;:",p2//6)
+print("Max no of sixes by 3rd:",p3//6)
