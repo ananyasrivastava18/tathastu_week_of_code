6f0f7c4e794560e0658378735af52fae9fc14ca0
ananyasrivastava18 <65857467+ananyasrivastava18@users.noreply.github.com>
2020-05-25 11:00:44 +0530

    Create Program1.py

---
+++ Program1.py
@@ -0,0 +1,13 @@
+name=input("Enter your name")
+branch=input("Enter your branch")
+gender=input("Enter your gender")
+college=input("Enter your college")
+age=input("Enter your age")
+print("Name=",name)
+print("Branch=", branch)
+print("Gender=",gender)
+print("College=",college)
+
+
+
+
